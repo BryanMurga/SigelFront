e80810f21a68cb5a450eab04231fd62c32824345
BryanMurga <20203tn041@utez.edu.mx>
2023-11-07 14:04:42 -0600

    Login funcion

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from '../views/Home.vue'
 import LoginView from '../views/Login.vue'
+import PruebaView from '../views/Prueba.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,6 +11,11 @@ const router = createRouter({
             path: '/',
             name: 'init',
             component: localStorage.token != undefined ? HomeView : LoginView,
+        },
+        {
+            path: '/prueba',
+            name: 'prueba',
+            component: localStorage != undefined ? PruebaView : LoginView,
         }
     ],
 })
---
+++ src/views/Prueba.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Login complete</h1>
+</template>
