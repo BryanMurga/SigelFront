07dfbd6d375b9e6edb4725139bb7759191d871c6
Milton Jaimes <20193tn028@utez.edu.mx>
2023-11-07 16:07:17 -0600

    side bar in progress

---
+++ src/components/sideBar.vue
@@ -0,0 +1,41 @@
+<template>
+    <link
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+  rel="stylesheet"
+/>
+
+    <div class="flex h-screen bg-gray-100">
+    <!-- Menú lateral -->
+    <div class="bg-gray-800 text-white w-64 flex flex-col">
+      <div class="image-container w-[50%]">
+        <!-- Agregar la imagen -->
+      </div>
+      <!-- Nombre de usuario -->
+      <div class="text-center py-4 text-lg font-bold">Nombre de Usuario</div>
+      <ul class="flex flex-col justify-center items-center">
+        <li class="py-5"><a href="#"><i class="fas fa-home" style="color: #48C9B0;" ></i>  Home</a></li>
+        <li class="py-5"><a href="#"><i class="fas fa-bars-progress" style="color: #48C9B0;"></i>  Leads</a></li>
+        <li class="py-5"><a href="#"><i class="fas fa-circle-plus" style="color: #48C9B0;"></i>  Asignar</a></li>
+        <li class="py-5"><a href="#"><i class="fas fa-user-slash" style="color: #48C9B0;"></i>  Reasignar</a></li>
+        <li class="py-5"><a href="#"><i class="fas fa-arrow-right-from-bracket" style="color: #48C9B0;"></i>  Sign Out</a></li>
+      </ul>
+    </div>
+
+    <!-- Contenido principal -->
+    <div class="flex-1 p-8">
+      <!-- Contenido principal de tu sitio web o aplicación -->
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+    .image-container{
+        background-image:  url(/logo.png);
+        background-position: center;
+        background-size: cover;
+        height: 16em;
+        width: 16em;
+    }
+</style>
+
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import HomeView from '../views/Home.vue'
 import LoginView from '../views/Login.vue'
+import RegisterView from '../views/Register.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,8 +11,23 @@ const router = createRouter({
             path: '/',
             name: 'init',
             component: localStorage.token != undefined ? HomeView : LoginView,
+        },
+        {
+            path: '/register',
+            name: 'register',
+            component: localStorage.token != undefined ? HomeView : RegisterView,
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: LoginView,
+        },
+        {
+            path: '/register',
+            name: 'register',
+            component: RegisterView,
         }
     ],
 })
 
-    export default router
+export default router
