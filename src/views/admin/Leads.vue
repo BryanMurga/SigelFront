544222127de8d9a32d4cccca233a7af356bfd4ce
BryanMurga <20203tn041@utez.edu.mx>
2023-11-17 09:30:13 -0600

    Listado de la vista de leads

--- src/views/admin/Leads.vue
+++ src/views/admin/Leads.vue
@@ -3,16 +3,42 @@
         <div class="lg:ml-64 p-4">
             <i class="fas fa-bars-progress text-2xl" style="color: #48c9b0"></i>
             <span id="posicion" class="ml-2 text-gray-500 dark:text-gray-400 text-lg">Leads</span>
+
+            <div class="flex-1 p-4">
+                <form>
+                    <label for="default-search"
+                        class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                    <div class="relative">
+                        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                            </svg>
+                        </div>
+                        <input v-model="input" type="search" id="default-search" name="leadSearch"
+                            class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="Buscar registros" required />
+                        <!-- <div class="items" v-for="lead in filterList()" :key=lead.id>
+                                <p>{{ lead.NombreCompleto }}</p>
+                            </div>
+                            <div class="item-error" v-if="input&&!filterList().length">
+                                <p>No se encontraron registros</p>
+                            </div> -->
+                    </div>
+                </form>
+            </div>
+
         </div>
         <SideBarADM class="lg:w-64 md:w-48 sm:w-32" /> <!-- Importa y utiliza el componente SidebarADM -->
-        <Search v-model="searchQuery"/>
 
     </header>
 
     <section>
         <div class="flex-1 lg:ml-64">
             <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <table v-if="filterList.length > 0"
+                    class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
                             <!-- <th scope="col" class="p-4">
@@ -129,24 +155,17 @@
                             </th>
                         </tr>
                     </thead>
-                    <tbody>
-                        <tr v-for="lead in leads" :key="lead.LeadID"
-                            class="px-6 py-4 font-medium text-gray-700 whitespace-nowrap dark:text-white">
-                            <!-- <td class="w-4 p-4">
-                                <div class="flex items-center">
-                                    <input id="checkbox-table-search-1" type="checkbox"
-                                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                    <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
-                                </div>
-                            </td> -->
+                    <tbody class="item leads">
+                        <tr v-for="lead in filterList" :key="lead.id"
+                            class="item leads px-6 py-4 font-medium text-gray-700 whitespace-nowrap dark:text-white">
                             <th class="px-6 py-4">
                                 {{ lead.NombreCompleto }}
                             </th>
                             <td class="px-6 py-4">
-                                {{ lead.Telefono }}
+                                {{ lead.telefono }}
                             </td>
                             <td class="px-6 py-4">
-                                {{ lead.Telefono2 }}
+                                {{ lead.telefono2 }}
                             </td>
                             <td class="px-6 py-4">
                                 {{ lead.CorreoElectronico }}
@@ -155,10 +174,10 @@
                                 {{ lead.CorreoElectronico2 }}
                             </td>
                             <td class="px-6 py-4">
-                                {{ lead.FechaPrimerContacto }}
+                                {{ formatDate(lead.FechaPrimerContacto) }}
                             </td>
                             <td class="px-6 py-4">
-                                {{ lead.FechaNac }}
+                                {{ formatDate(lead.FechaNac) }}
                             </td>
                             <td class="px-6 py-4">
                                 {{ lead.EscuelaProcedencia }}
@@ -176,7 +195,7 @@
                                 {{ lead.PSeguimiento }}
                             </td>
                             <td class="px-6 py-4">
-                                {{ lead.CarreraInteresID }}
+                                {{ lead.CarreraInteres }}
                             </td>
                             <td class="px-6 py-4">
                                 {{ lead.Grado }}
@@ -194,7 +213,7 @@
                                 {{ lead.Ciclo }}
                             </td>
                             <td class="px-6 py-4">
-                                {{ lead.CampanaID }}
+                                {{ lead.NombreCampana }}
                             </td>
                             <td class="px-6 py-4">
                                 {{ lead.AsetNameForm }}
@@ -203,7 +222,7 @@
                                 {{ lead.IsOrganic }}
                             </td>
                             <td class="px-6 py-4">
-                                {{ lead.MedioDeContactoID }}
+                                {{ lead.MedioContacto }}
                             </td>
                             <td class="px-6 py-4">
                                 {{ lead.TipoReferido }}
@@ -215,10 +234,10 @@
                                 {{ lead.DondeObtDato }}
                             </td>
                             <td class="px-6 py-4">
-                                {{ lead.FechaInscripcion }}
+                                {{ formatDate(lead.FechaInscripcion) }}
                             </td>
                             <td class="px-6 py-4">
-                                {{ lead.CarreraInscripcion }}
+                                {{ lead.CarreraInscrita }}
                             </td>
                             <td class="px-6 py-4">
                                 {{ lead.BecaOfrecida }}
@@ -227,16 +246,16 @@
                                 {{ lead.NumeroLista }}
                             </td>
                             <td class="px-6 py-4">
-                                {{ lead.PromotorOriginal }}
+                                {{ lead.NombrePromotorOri }}
                             </td>
                             <td class="px-6 py-4">
-                                {{ lead.FechaPromotorOriginal }}
+                                {{ formatDate(lead.FechaPromotorOriginal) }}
                             </td>
                             <td class="px-6 py-4">
-                                {{ lead.PromotorActual }}
+                                {{ lead.NombrePromotorAct }}
                             </td>
                             <td class="px-6 py-4">
-                                {{ lead.FechaPromotorActual }}
+                                {{ formatDate(lead.FechaPromotorActual) }}
                             </td>
                             <td class="px-6 py-4">
                                 {{ lead.Comentarios }}
@@ -245,10 +264,13 @@
                                 {{ lead.Contacto }}
                             </td>
                         </tr>
-
-
                     </tbody>
                 </table>
+
+                <div class="item-error" v-if="input && !filterList.length">
+                    <p class="p-4">No hay coicidencia de registros</p>
+                </div>
+
             </div>
         </div>
     </section>
@@ -261,38 +283,41 @@ import { getUserName } from "../../sessions";
 import SideBarADM from "../../components/SideBarADM.vue";
 import Search from "../../components/Search.vue";
 import axios from "axios";
-
+import { ref } from "vue";
+import { format } from "date-fns";
 // initialize components based on data attribute selectors
 onMounted(() => {
     initFlowbite();
 });
 
 export default {
+
     data() {
         return {
             userName: getUserName(),
             leads: [],
-            searchQuery: '',
+            buscador: '',
+            setTimeoutBuscador: '',
+            input: ref(''),
         };
 
     },
     computed: {
-        filteredLeads() {
-            // Filter leads based on the search query
-            return this.leads.filter((lead) => {
-                return Object.values(lead).some((value) => {
-                    if (value) {
-                        const stringValue = String(value).toLowerCase();
-                        const lowercasedQuery = this.searchQuery.toLowerCase();
-                        return stringValue.includes(lowercasedQuery);
-                    }
-                    return false;
-                });
+        filterList() {
+
+            if (this.leads.length === 0) {
+                return [];
+            }
+
+            return this.leads.filter(lead => {
+                const nombreCompleto = lead && lead.NombreCompleto ? lead.NombreCompleto : "";
+                return nombreCompleto.toLowerCase().includes(this.input.toLowerCase());
             });
         },
     },
     mounted() {
         this.loadLeads();
+
         // Agrega un nuevo estado al historial cuando el componente se monta
         window.history.pushState({ noBackExitsApp: true }, null, null);
 
@@ -308,7 +333,10 @@ export default {
             try {
                 // Reemplaza 'tu-endpoint' con la URL real de tu endpoint
                 const response = await axios.get('http://localhost:4000/leads');
-                this.leads = response.data.leads; // Almacena los leads en el array
+                if (response.data && response.data.leads) {
+                    this.leads = response.data.leads; // Almacena los leads en el array
+                }
+
             } catch (error) {
                 console.error('Error al obtener leads:', error);
             }
@@ -321,7 +349,29 @@ export default {
             } else {
                 window.history.pushState({ noBackExitsApp: true }, null, null);
             }
-        }
+        },
+        formatDate(date) {
+            if(date){
+                const formattedDate = format(new Date(date), "dd/MM/yyyy");
+                return formattedDate;
+            }else{
+                return '';
+            }
+            // Formatea la fecha utilizando date-fns
+            
+        },
+        
+    },
+    watch: {
+        leads: {
+            handler() {
+                // Este método se ejecutará cada vez que 'leads' cambie
+                // Puedes realizar cualquier lógica adicional aquí
+                this.filterList; // Llamar a filterList cuando leads cambie
+                this.formatDate; // Llamar a formatDate cuando leads cambie
+            },
+        },
+        inmediate: true,
     },
     components: { SideBarADM, Search },
 };
