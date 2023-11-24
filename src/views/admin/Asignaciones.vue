ac5c8cb42aecc81e58f7be487af6e9bd46e34f16
Milton Jaimes <20193tn028@utez.edu.mx>
2023-11-24 13:33:30 -0600

    Asignaciones and reasiganciones detallitos

--- src/views/admin/Asignaciones.vue
+++ src/views/admin/Asignaciones.vue
@@ -15,21 +15,21 @@
         <div class="flex-1 lg:ml-64">
             <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
                 <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400" style="background-color: #48C9B0;">
                         <tr>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" class="px-6 py-3 text-white">
                                 ID Lead
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" class="px-6 py-3 text-white">
                                 Nombre Completo
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" class="px-6 py-3 text-white">
                                 Telefono
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" class="px-6 py-3 text-white">
                                 Correo Electronico
                             </th>
-                            <th scope="col" class="px-6 py-3">
+                            <th scope="col" class="px-6 py-3 text-white">
                                 Promotor
                             </th>
                         </tr>
@@ -65,12 +65,13 @@
                 </table>
                 <br>
                 <button type="button"
+                    v-if="hayLeadsConPromotorSeleccionado"
                     class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
                     style="text-align: left; float: right;" @click="enviarAsignaciones">
                     <i class="fas fa-regular fa-paper-plane"></i> Enviar
                 </button>
-                <div class="grid justify-items-center" v-if="!leads.length">
-                    <p>No hay leads para asignar</p>
+                <div class="grid justify-items-center" v-if="!leads.length" style="background-color: #F4D03F;">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-500 dark:text-grey">No hay leads para asignar</h5>
                 </div>
             </div>
         </div>
@@ -95,6 +96,12 @@ onMounted(() => {
 });
 
 export default {
+
+    computed: {
+    hayLeadsConPromotorSeleccionado() {
+      return this.leads.some(lead => lead.selectedPromotor !== null);
+    }
+  },
 
     //types of toast
    setup() {
