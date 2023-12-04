1c980c58e97ab5e1b9a4f64b6708be832afc1c7c
Milton Jaimkes <20193tn028@utez.edu.mx>
2023-12-04 14:25:36 -0600

    troleito

--- src/views/promotor/Leads.vue
+++ src/views/promotor/Leads.vue
@@ -106,7 +106,7 @@
                                             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre</label>
                                         <input type="text" name="name" id="name"
                                             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                                            v-model="inputValue" placeholder="name" required="">{{ lead.NombreCompleto }}
+                                            v-model="inputValue" placeholder="name" required="">
                                     </div>
                                     <div class="col-span-2 sm:col-span-1">
                                         <label for="price"
@@ -669,11 +669,11 @@ export default {
                     console.error('Lead no encontrado');
                     return;
                 }
-
+                
                 // Aquí puedes asignar la información del lead al modal o a otras variables para mostrarla en el modal
                 this.inputValue = lead.NombreCompleto;
                 // Puedes realizar otras acciones, como mostrar el modal o asignar la información a variables del modal
-
+                this.loadContactos(LeadID);
             } catch (error) {
                 // Manejar errores de la solicitud HTTP
                 console.error('Error al obtener el lead:', error);
