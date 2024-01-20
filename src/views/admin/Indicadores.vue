23c51b72832e0420de29cdebb135bcecf69f3d4d
Milton Jaimkes <20193tn028@utez.edu.mx>
2024-01-20 13:09:39 -0600

    Cambios indicadores

--- src/views/admin/Indicadores.vue
+++ src/views/admin/Indicadores.vue
@@ -1,26 +1,11 @@
 <template>
   <div>
     <div class="lg:ml-60 p-4">
-      <i class="fas fa-bars-progress text-2xl" style="color: #48c9b0"></i>
-      <span id="posicion" class="ml-2 text-gray-500 dark:text-gray-400 text-lg">Promotores</span>
+      <i class="fa-solid fa-arrow-trend-up text-2xl" style="color: #48c9b0"></i>
+      <span id="posicion" class="ml-2 text-gray-500 dark:text-gray-400 text-lg">Indicadores</span>
     </div>
 
     <SideBarADM />
-
-    <div class="mt-6 ml-64 mr-10 mb-5">
-      <div class="relative">
-        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-          <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-            fill="none" viewBox="0 0 20 20">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-          </svg>
-        </div>
-        <input v-model="searchQuery" type="search" name="leadSearch"
-          class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Buscar registros" required />
-      </div>
-    </div>
 
     
 
