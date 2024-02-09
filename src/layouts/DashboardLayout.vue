8ff9a065f30136f828711dbb618726cd2432571a
jon013002 <jonmtz3002@gmail.com>
2024-02-09 10:50:51 -0600

    Add: Nuevo layout para dashboard

---
+++ src/layouts/DashboardLayout.vue
@@ -0,0 +1,233 @@
+<script>
+import { getRole, getUserName, clearRole, clearUserName } from "../sessions";
+import SideBarLink from './../components/SideBarLink.vue';
+
+export default {
+    props: {
+        name: String,
+    },
+    data() {
+        return {
+            selectedSection: null,
+            userName: getUserName(),
+            role: '',
+        };
+    },
+    created() {
+        this.role = getRole();
+    },
+    computed: {
+        isAdmin() {
+            return this.role === 'admin';
+        },
+        isPromotor() {
+            return this.role === 'promotor';
+        },
+        isCoordinador() {
+            return this.role === 'coordinador';
+        }
+    },
+    methods: {
+        // Identifica la ruta actual
+        isLinkActive(path) {
+            return this.$route.path === path;
+        },
+        selectSection(section) {
+            this.selectedSection = section;
+        },
+        cerrarSesion() {
+            clearRole();
+            clearUserName();
+            this.$router.push("/login");
+        },
+    },
+    components: {
+        SideBarLink
+    }
+};
+</script>
+
+<template>
+    <!-- component -->
+    <aside
+        class="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-[#1f2937] text-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
+        <div>
+            <div class="mt-8 text-center">
+                <img src="/logo.png" alt="" class="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28">
+                <h5 class="hidden mt-4 text-xl font-semibold lg:block">
+                    ¡Bienvenido! {{ userName }}
+                </h5>
+                <!-- <span class="hidden text-gray-400 lg:block">Admin</span> -->
+            </div>
+
+            <ul class="space-y-2 tracking-wide mt-8">
+                <!-- Enlaces para admin -->
+                <li v-if="isAdmin">
+                    <SideBarLink href="home-adm" :active="isLinkActive('/home-adm')">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                            <path class="fill-current group-hover:text-[#4fc4ae]"
+                                d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
+                            <path class="fill-current group-hover:text-[#4fc4ae]"
+                                d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
+                        </svg>
+                        <span class="-mr-1 font-medium">Inicio</span>
+                    </SideBarLink>
+                </li>
+                <li v-if="isAdmin">
+                    <SideBarLink href="leads" :active="isLinkActive('/leads')">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6">
+                            <path class="fill-current group-hover:text-[#4fc4ae]" stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
+                        </svg>
+                        <span class="group-hover:text-[#4fc4ae]">Leads</span>
+                    </SideBarLink>
+                </li>
+                <li v-if="isAdmin">
+                    <SideBarLink href="asignaciones" :active="isLinkActive('/asignaciones')">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                            <path class=" group-hover:text-[#4fc4ae]" fill-rule="evenodd"
+                                d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z"
+                                clip-rule="evenodd" />
+                        </svg>
+                        <span class="group-hover:text-[#4fc4ae]">Asignar</span>
+                    </SideBarLink>
+                </li>
+                <li v-if="isAdmin">
+                    <SideBarLink href="reasignaciones" :active="isLinkActive('/reasignaciones')">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                            <path class=" group-hover:text-[#4fc4ae]"
+                                d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" />
+                            <path class=" group-hover:text-[#4fc4ae]"
+                                d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z" />
+                        </svg>
+                        <span class="group-hover:text-[#4fc4ae]">Reasignar</span>
+                    </SideBarLink>
+                </li>
+                <li v-if="isAdmin">
+                    <SideBarLink href="cargar-archivo" :active="isLinkActive('/cargar-archivo')">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                            <path class=" group-hover:text-[#4fc4ae]" fill-rule="evenodd"
+                                d="M10.5 3.75a6 6 0 0 0-5.98 6.496A5.25 5.25 0 0 0 6.75 20.25H18a4.5 4.5 0 0 0 2.206-8.423 3.75 3.75 0 0 0-4.133-4.303A6.001 6.001 0 0 0 10.5 3.75Zm2.03 5.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 1 0 1.06 1.06l1.72-1.72v4.94a.75.75 0 0 0 1.5 0v-4.94l1.72 1.72a.75.75 0 1 0 1.06-1.06l-3-3Z"
+                                clip-rule="evenodd" />
+                        </svg>
+                        <span class="group-hover:text-[#4fc4ae]">Cargar base</span>
+                    </SideBarLink>
+                </li>
+                <!-- Fin de enlaces admin -->
+
+                <!-- Enlaces para promotor -->
+                <li v-if="isPromotor">
+                    <a href="dash-promotor" aria-label="dashboard"
+                        class="relative px-4 py-3 flex items-center space-x-4 rounded-xl text-white bg-[#057a55]">
+                        <svg class="-ml-1 h-6 w-6" viewBox="0 0 24 24" fill="none">
+                            <path
+                                d="M6 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V8ZM6 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2v-1Z"
+                                class="fill-current text-cyan-400 dark:fill-slate-600"></path>
+                            <path d="M13 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2V8Z"
+                                class="fill-current text-cyan-200 group-hover:text-cyan-300"></path>
+                            <path d="M13 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-1Z"
+                                class="fill-current group-hover:text-sky-300"></path>
+                        </svg>
+                        <span class="-mr-1 font-medium">Inicio</span>
+                    </a>
+                </li>
+                <li v-if="isPromotor">
+                    <a href="leads-promotor" class="px-4 py-3 flex items-center space-x-4 rounded-md group">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6">
+                            <path class="fill-current group-hover:text-[#4fc4ae]" stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
+                        </svg>
+                        <span class="group-hover:text-[#4fc4ae]">Leads</span>
+                    </a>
+                </li>
+                <li v-if="isPromotor">
+                    <a href="registrar-lead" class="px-4 py-3 flex items-center space-x-4 rounded-md group">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                            <path class=" group-hover:text-[#4fc4ae]" fill-rule="evenodd"
+                                d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z"
+                                clip-rule="evenodd" />
+                        </svg>
+                        <span class="group-hover:text-[#4fc4ae]">Registrar</span>
+                    </a>
+                </li>
+                <li v-if="isPromotor">
+                    <a href="alumno-promotor" class="px-4 py-3 flex items-center space-x-4 rounded-md group">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                            <path class=" group-hover:text-[#4fc4ae]" fill-rule="evenodd"
+                                d="M7.502 6h7.128A3.375 3.375 0 0 1 18 9.375v9.375a3 3 0 0 0 3-3V6.108c0-1.505-1.125-2.811-2.664-2.94a48.972 48.972 0 0 0-.673-.05A3 3 0 0 0 15 1.5h-1.5a3 3 0 0 0-2.663 1.618c-.225.015-.45.032-.673.05C8.662 3.295 7.554 4.542 7.502 6ZM13.5 3A1.5 1.5 0 0 0 12 4.5h4.5A1.5 1.5 0 0 0 15 3h-1.5Z"
+                                clip-rule="evenodd" />
+                            <path class=" group-hover:text-[#4fc4ae]" fill-rule="evenodd"
+                                d="M3 9.375C3 8.339 3.84 7.5 4.875 7.5h9.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 0 1 3 20.625V9.375Zm9.586 4.594a.75.75 0 0 0-1.172-.938l-2.476 3.096-.908-.907a.75.75 0 0 0-1.06 1.06l1.5 1.5a.75.75 0 0 0 1.116-.062l3-3.75Z"
+                                clip-rule="evenodd" />
+                        </svg>
+                        <span class="group-hover:text-[#4fc4ae]">Inscritos</span>
+                    </a>
+                </li>
+                <!-- Fin de enlaces promotor -->
+
+                <!-- Enlaces de coordinador -->
+                <li v-if="isCoordinador">
+                    <a href="alumno-coordinador" aria-label="dashboard"
+                        class="relative px-4 py-3 flex items-center space-x-4 rounded-xl text-white bg-[#057a55]">
+                        <svg class="-ml-1 h-6 w-6" viewBox="0 0 24 24" fill="none">
+                            <path
+                                d="M6 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V8ZM6 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2v-1Z"
+                                class="fill-current text-cyan-400 dark:fill-slate-600"></path>
+                            <path d="M13 8a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2V8Z"
+                                class="fill-current text-cyan-200 group-hover:text-cyan-300"></path>
+                            <path d="M13 15a2 2 0 0 1 2-2h1a2 2 0 0 1 2 2v1a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-1Z"
+                                class="fill-current group-hover:text-sky-300"></path>
+                        </svg>
+                        <span class="-mr-1 font-medium">Alumnos</span>
+                    </a>
+                </li>
+                <li v-if="isCoordinador">
+                    <a href="alumno-promotor" class="px-4 py-3 flex items-center space-x-4 rounded-md group">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                            <path class=" group-hover:text-[#4fc4ae]" fill-rule="evenodd"
+                                d="M7.502 6h7.128A3.375 3.375 0 0 1 18 9.375v9.375a3 3 0 0 0 3-3V6.108c0-1.505-1.125-2.811-2.664-2.94a48.972 48.972 0 0 0-.673-.05A3 3 0 0 0 15 1.5h-1.5a3 3 0 0 0-2.663 1.618c-.225.015-.45.032-.673.05C8.662 3.295 7.554 4.542 7.502 6ZM13.5 3A1.5 1.5 0 0 0 12 4.5h4.5A1.5 1.5 0 0 0 15 3h-1.5Z"
+                                clip-rule="evenodd" />
+                            <path class=" group-hover:text-[#4fc4ae]" fill-rule="evenodd"
+                                d="M3 9.375C3 8.339 3.84 7.5 4.875 7.5h9.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-9.75A1.875 1.875 0 0 1 3 20.625V9.375Zm9.586 4.594a.75.75 0 0 0-1.172-.938l-2.476 3.096-.908-.907a.75.75 0 0 0-1.06 1.06l1.5 1.5a.75.75 0 0 0 1.116-.062l3-3.75Z"
+                                clip-rule="evenodd" />
+                        </svg>
+                        <span class="group-hover:text-[#4fc4ae]">Registrar</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="px-6 -mx-6 pt-4 flex justify-between items-center border-t">
+            <a @click="cerrarSesion"
+                class="px-4 py-3 flex items-center space-x-4 rounded-md hover:text-[#4fc4ae]  group cursor-pointer">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+                </svg>
+                <span class="hover:text-[#4fc4ae]">Cerrar Sesión</span>
+            </a>
+        </div>
+    </aside>
+    <div class="ml-auto lg:w-[75%] xl:w-[80%] 2xl:w-[85%]">
+        <div class="sticky z-10 top-0 h-16 border-b bg-white lg:py-2.5">
+            <div class="px-6 flex items-center justify-between space-x-4 2xl:container">
+                <h5 hidden class="text-2xl text-gray-600 font-medium lg:block">{{ name }}</h5>
+                <button class="w-12 h-16 -mr-2 border-r lg:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 my-auto" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                    </svg>
+                </button>
+            </div>
+        </div>
+
+        <div class="px-6 py-6 2xl:container h-[calc(100vh-64px)]">
+            <slot />
+        </div>
+    </div>
+</template>
