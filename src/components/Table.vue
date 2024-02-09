9ce94d6551dda61f4eaa7b82bb6f29cb8b3853b4
jon013002 <jonmtz3002@gmail.com>
2024-02-09 12:55:02 -0600

    Add new components of Table

---
+++ src/components/Table.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="relative overflow-x-auto pb-6">
+        <table class="w-full text-sm text-left text-gray-500">
+            <thead class="text-xs text-white capitalize bg-sky-800">
+                <slot name="header" />
+            </thead>
+            <tbody class="text-center border-x"> 
+                <slot></slot>
+            </tbody>
+        </table>
+    </div>
+</template>
