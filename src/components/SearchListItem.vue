d36f85e2fd91c9f8ebba013169ea523c94658f20
jon013002 <jonmtz3002@gmail.com>
2024-02-17 13:01:09 -0600

    Add new components

---
+++ src/components/SearchListItem.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      required: true
+    },
+    onClick: {
+      type: Function,
+      required: true
+    }
+  },
+  methods: {
+    handleClick() {
+      this.onClick(this.label);
+    }
+  }
+};
+</script>
+
+
+<template>
+    <li>
+        <button 
+            type="button"
+            class="dropdown-item inline-flex w-full px-4 py-2 hover:bg-gray-100"
+            @click="handleClick"
+        >
+            {{ label }}
+        </button>
+    </li>
+</template>
