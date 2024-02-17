8fed47cf49a3b3be5aba50633e7ac16b3eca4aed
jon013002 <jonmtz3002@gmail.com>
2024-02-17 12:56:37 -0600

    Add new font-family

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router';
 import Swal from 'sweetalert2'
+import '@fontsource-variable/onest';
 </script>
 
 <template>
   <RouterView />  
 </template>
+
+<style>
+body{
+  font-family: 'Onest Variable', sans-serif;
+}
+</style>
 
