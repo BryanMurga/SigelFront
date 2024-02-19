fcf9c68f474f6dec16d88cb9da924e267e53da3d
jon013002 <jonmtz3002@gmail.com>
2024-02-19 10:35:27 -0600

    comentarios baja

--- src/views/coordinador/AlumnoCoordinador.vue
+++ src/views/coordinador/AlumnoCoordinador.vue
@@ -332,7 +332,7 @@
                     <!-- Modal header -->
                     <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
                         <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                            Baja Alumno SEGURO?
+                            Baja Alumno
                         </h3>
                         <button type="button"
                             class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
@@ -356,6 +356,13 @@
                                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                         <option v-for="est in tipoBaja" :key="est" :value="est">{{ est }}</option>
                                     </select>
+                            </div>
+                            <div class="col-span-2 sm:col-span-1">
+                                <label for="comentario"
+                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Comentario</label>
+                                <textarea id="comentario" rows="4"
+                                    class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    placeholder="Agrega el motivo de la baja"></textarea>
                             </div>
                         </div>
                         
@@ -815,10 +822,6 @@ export default {
                 this.errUpdate();
             }
             this.loadAlumnos();
-<<<<<<< HEAD
-            
-=======
->>>>>>> 93fab6157a2e76398b98965f0769ca6d202025b9
         },
 
         hayCambiosEnDatos() {
